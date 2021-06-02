bd422fe963c3116c6289c124635a52b35b5e6e12
nidaj <nida.jawre@gmail.com>
2021-06-02 14:57:30 +0530

    Delete harmonic_number.py

--- harmonic_number.py
+++
@@ -1,8 +0,0 @@
-"""
-Program to accept a number n and print harmonic value of n
-"""
-n = int(input("Enter the number: "))
-harmonic_value = 1.00
-for i in range(2, n + 1):
-    harmonic_value += 1 / i
-print(f"The harmonic value of {n} = {round(harmonic_value, 2)}")
