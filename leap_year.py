acc978f53c92b56fc331c2f8a97db4816db26ac3
nidaj <nida.jawre@gmail.com>
2021-06-02 14:57:44 +0530

    Delete leap_year.py

--- leap_year.py
+++
@@ -1,9 +0,0 @@
-year = input("Enter the year: ")
-if len(year) < 4:
-    print("The year entered should be a 4 digit number")
-else:
-    year = int(year)
-    if ((year % 400) == 0) or ((year % 4) == 0) and ((year % 100) != 0):
-        print(f"{year} is a Leap Year")
-    else:
-        print(f"{year} is not a leap year")
